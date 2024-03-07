0048baac82a1c216545af28178cc627e638a6765
emrehangorgec <emrehan_gorgec@hotmail.com>
2024-03-07 12:13:11 +0300

    -

--- RSA.py
+++ RSA.py
@@ -1,10 +1,8 @@
-# Implementation of the RSA algorithm without using external libraries
-# to generate digital signature and verification
-
 import math
 import random
 
 
+# Function to check whether the given number is prime or not.
 def is_prime(number):
     if number < 2:
         return False
@@ -14,10 +12,12 @@ def is_prime(number):
     return True
 
 
+# Function to create a random integer between the given interval
 def randint(min_value, max_value):
     return random.randint(min_value, max_value)
 
 
+# Function to generate random prime numbers between the given interval
 def generate_prime(min_value, max_value):
     prime = randint(min_value, max_value)
     while not is_prime(prime):
@@ -25,9 +25,10 @@ def generate_prime(min_value, max_value):
     return prime
 
 
-def mod_inverse(e, totient):
+# Find the "d" value which is equivalent to the e x d = 1 mod(totient_n)
+def mod_inverse(e, totient_n):
     d = 3
-    while (d * e) % totient != 1:
+    while (d * e) % totient_n != 1:
         d += 1
     return d
 
@@ -40,38 +41,42 @@ def sign(message, d, n):
 
 
 # Decrypt the message by using public key, decode the decrypted message.
+# Return the result of the verification process.
 def verify(signature, e, n, message):
     decrypted_signature = [pow(i, e, n) for i in signature]
     decoded_message = "".join([chr(i) for i in decrypted_signature])
     return decoded_message == message
 
 
-# Key Generation
+# Key Generation for the RSA
 p, q = generate_prime(3, 5000), generate_prime(3, 5000)
 while p == q:
     q = generate_prime(3, 47)
 
 n = p * q
-totient = (p - 1) * (q - 1)
+totient_n = (p - 1) * (q - 1)
 
-e = randint(3, totient - 1)
+# Find a number "e" that is coprime to the "totient_n"
+e = randint(3, totient_n - 1)
 
-while math.gcd(e, totient) != 1:
-    e = randint(3, totient - 1)
+while math.gcd(e, totient_n) != 1:
+    e = randint(3, totient_n - 1)
 
-d = mod_inverse(e, totient)
+d = mod_inverse(e, totient_n)
 # End of the Key Generation
 
 
-print(f"\nPublic Key:{e}\nPrivate Key:{d}\nn:{n}\nPhi of n:{totient}\np:{p}\nq:{q}\n\n")
+print(
+    f"\nPublic Key:{e}\nPrivate Key:{d}\nn:{n}\nPhi of n:{totient_n}\np:{p}\nq:{q}\n\n"
+)
 
 message = input("Enter the message: ")
 
-# Sign the message with private key.
+# Sign the message with the private key
 signature = sign(message, d, n)
 
 
-# Verify the digital signature with public key
+# Verify the digital signature with the public key
 _signature = verify(signature, e, n, message)
 signed_message = message + " | " + " ".join(map(str, signature))
 
